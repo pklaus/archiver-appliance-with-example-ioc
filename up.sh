6e8eeb494895fc463dcbcbbc4a5f31120bc6cbf5
Philipp Klaus <philipp.l.klaus@web.de>
2020-06-16 13:01:09 +0200

    up: open firefox once the tomcat server is running

--- up.sh
+++ up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-firefox "http://localhost:17665/mgmt/ui/index.html"
+# Open the management page in 20s from now:
+(sleep 20; firefox "http://localhost:17665/mgmt/ui/index.html") &
 
 docker-compose up
